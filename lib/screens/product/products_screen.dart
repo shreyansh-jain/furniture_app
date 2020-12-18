026832d22c1be997a36d5b69113ed52b752b1397
shreyansh-jain <shreyanshbafna.310@gmail.com>
2020-12-18 07:37:35 +0530

    Added Basic Dependency and Project Structure

---
+++ lib/screens/product/products_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
